1ddd212ca4070f9bbe36ef66fda0351bfe3b8f18
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-01 19:08:59 -0500

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,34 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  // DO YOUR
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`log: ${timestamp} / ${method} / ${url}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  // DO YOUR
+  console.log("validateUserId");
+  next();
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  // DO YOUR
+  console.log("validateUser");
+  next();
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  // DO YOUR
+  console.log("validatePost");
+  next();
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+};
