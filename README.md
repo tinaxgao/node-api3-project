002a5cacc49558b0c27c15344b3bd32dfb635f09
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-01 20:07:50 -0500

    progress

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Your assignment page on Canvas should contain instructions for submitting this p
 
 - `logger()`
 
-  - `logger` logs to the console the following information about each request: request method, request url, and a timestamp
+  - `logger` logs to the console the following information about each request: request method, request (originalUrl) url, and a timestamp
   - this middleware runs on every request made to the API
 
 - `validateUserId()`
--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,28 +1,45 @@
+const User = require("../users/users-model");
+
 function logger(req, res, next) {
-  // DO YOUR
-  const timestamp = new Date().toLocaleString()
-  const method = req.method
-  const url = req.originalUrl
+  const timestamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
   console.log(`log: ${timestamp} / ${method} / ${url}`);
   next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR
-  console.log("validateUserId");
-  next();
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id);
+    if (!user) {
+      res.status(404).json({ message: "user not found" });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({ message: "uh oh problems finding user" });
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR
-  console.log("validateUser");
-  next();
+  const { name } = req.body;
+  if (!name || !name.trim()) {
+    res.status(400).json({ message: "missing required name field" });
+  } else {
+    req.name = name.trim();
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR
-  console.log("validatePost");
-  next();
+  const { text } = req.body;
+  if (!text || !text.trim()) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    req.text = text.trim();
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
