38eefe5cf7b0f2111fc56cc09c1b1964bf17bff9
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-02 00:25:24 -0500

    progress

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -31,16 +31,20 @@ router.post("/", validateUser, (req, res, next) => {
 });
 
 router.put("/:id", validateUserId, validateUser, (req, res, next) => {
- User.update(req.params.id, {name: req.name})
- .then(user => {
-   res.json(user)
- })
- .catch(next)
+  User.update(req.params.id, { name: req.name })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch(next);
 });
 
-router.delete("/:id", validateUserId, (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
+router.delete("/:id", validateUserId, async (req, res, next) => {
+  try {
+    await User.remove(req.params.id);
+    res.json(req.user);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/:id/posts", validateUserId, (req, res) => {
